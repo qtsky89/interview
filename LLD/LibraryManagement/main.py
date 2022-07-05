f39acc332ff7deba2dd4eb50f1e8ce33d9730372
Wonhee Jang <Qtsky89@gmail.com>
2022-07-05 19:53:32 +0900

    library done (skip some of it)

--- LLD/LibraryManagement/main.py
+++ LLD/LibraryManagement/main.py
@@ -1,7 +1,8 @@
 #!/bin/env python
+from collections import defaultdict
 import sys
 from enum import Enum, auto
-from typing import List
+from typing import List, DefaultDict, Dict
 
 # data class
 class User:
@@ -9,6 +10,8 @@ class User:
         self.id = id
         self.name = name
         self.address = address
+        self.checkout_count: int = 0
+        self.fine: int = 0 
 
 class Category(Enum):
     SCIENCE = 0
@@ -16,13 +19,17 @@ class Category(Enum):
     ENGLISH = auto()
 
 # data class
+book_id = 0
 class Book:
-    def __init__(self, title: str, author: str, category: Category, rack_number: int, publication_date: int):
+    def __init__(self, title: str, author: str, category: Category, rack_number: int, publication_date: int, count: int):
+        book_id += 1
+        self.id = book_id
         self.title = title
         self.author = author
         self.category = category
         self.rack_number = rack_number
         self.pulication_date = publication_date
+        self.count = count
 
 class BookShelve:
     def __init__(self):
@@ -39,6 +46,45 @@ class BookShelve:
 
     def search_book_by_publication_date(self, publication_date: int) -> List[Book]:
         return list(filter(lambda x:x.publication_date==publication_date, self.book))
+
+class CheckoutInfo:
+    def __init__(self, book: Book, date: int):
+        self.book = book
+        self.date = int
+
+
+class CheckoutSystem:
+    def __init__(self):
+        self.checkoutinfos: DefaultDict[User:List[CheckoutInfo]] = defaultdict()
+        
+    def checkout_book(self, user: User, book: Book):
+        if user.checkout_count > 5:
+            raise Exception(f'user {user} checkout count is larget than 5')
+        elif book.count == 0:
+            raise Exception('book is not available')
+
+        book.count -= 1
+        user.checkout_count += 1
+        self.checkoutinfos[user].append(book)
+    
+    # run once a day
+    def put_fine(self):
+        current_date = curret_date()
+        for user in self.checkoutinfos:
+            if self.checkoutinfos[user] + 1000 < current_date:
+                user.fine += 10
+            
+class ReserveSystem:
+    def __init__(self):
+        self.reserve_user_book: Dict[User, Book] = {}
+        self.reserve_book_user: Dict[Book, User] = {}
+    
+    def reserve_book(self, user: User, book: Book):
+        if book.count != 0:
+            raise Exception('book count is not zero. checkout instead')
+    
+        self.reserve_user_book[user] = book
+        self.reserve_book_user[book] = user
 
 
 def main(argv):
